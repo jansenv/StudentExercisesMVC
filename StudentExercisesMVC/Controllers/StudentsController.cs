5da46853949f35fcec0eb392c50e2b8636d5727e
jansenv <jansenv44@gmail.com>
2020-04-09 12:09:42 -0500

    figured out create and delete views

--- StudentExercisesMVC/Controllers/StudentsController.cs
+++ StudentExercisesMVC/Controllers/StudentsController.cs
@@ -153,8 +153,8 @@ namespace StudentExercisesMVC.Controllers
         // GET: Students/Delete/5
         public ActionResult Delete(int id)
         {
-
-            return View();
+            var student = GetStudentById(id);
+            return View(student);
         }
 
         // POST: Students/Delete/5
@@ -164,11 +164,21 @@ namespace StudentExercisesMVC.Controllers
         {
             try
             {
-                // TODO: Add delete logic here
+                using (SqlConnection conn = Connection)
+                {
+                    conn.Open();
+                    using (SqlCommand cmd = conn.CreateCommand())
+                    {
+                        cmd.CommandText = "DELETE FROM Student WHERE Id = @id";
+                        cmd.Parameters.Add(new SqlParameter("@id", id));
+
+                        cmd.ExecuteNonQuery();
+                    }
+                }
 
                 return RedirectToAction(nameof(Index));
             }
-            catch
+            catch (Exception ex)
             {
                 return View();
             }
